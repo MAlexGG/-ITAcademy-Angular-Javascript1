5a6d669887fda873977b98d74ce1e0984c721b15
Alexandra Galarza <alexandragalarzag@gmail.com>
2021-12-17 14:56:12 +0100

    Sprint Solution

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,57 @@
 // Get the input fields
 var password = document.querySelector(".password");
 var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+var firstName = document.querySelector('.name');
+var lastName = document.querySelector('.lastName');
+var address = document.querySelector('.address');
+var email = document.querySelector('.email');
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
+var errorLastName = document.getElementById('errorLastName');  
 var errorPhone = document.getElementById('errorPhone');  
+var errorAddress = document.getElementById('errorAddress');
+var errorMail = document.getElementById('errorMail');
+
 
 // Exercise 8
-function validate() {
+function validate(e) {
     // Validate fields entered by the user: name, phone, password, and email
+
+    let numbers = /^[0-9]+$/;
+    let letters = /^[A-Za-z]+$/;
+    let numbersAndLetters = /^([0-9]+[a-zA-Z]+|[a-zA-Z]+[0-9]+)[0-9a-zA-Z]*$/;
+    let emailValidation = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (password.value.length === 0 || password.value == null || password.value.length < 3 || !numbersAndLetters.test(password.value)) {
+        e.preventDefault();
+        errorPassword.style.display = 'block';
+        password.classList.add('invalidInput');
+    };
+    if (firstName.value.length === 0 || firstName.value == null || firstName.value.length < 3 || numbers.test(firstName.value)) {
+        e.preventDefault();
+        errorName.style.display = 'block';
+        firstName.classList.add('invalidInput');
+    };
+    if (lastName.value.length === 0 || lastName.value == null || lastName.value.length < 3 || numbers.test(lastName.value)) {
+        e.preventDefault();
+        errorLastName.style.display = 'block';
+        lastName.classList.add('invalidInput');
+    };
+    if (phone.value.length === 0 || phone.value == null || phone.value.length < 3 || letters.test(phone.value)) {
+        e.preventDefault();
+        errorPhone.style.display = 'block';
+        phone.classList.add('invalidInput');
+    };
+    if (address.value.length === 0 || address.value == null || address.value.length < 3 ) {
+        e.preventDefault();
+        errorAddress.style.display = 'block';
+        address.classList.add('invalidInput');
+    };
+    if (email.value.length === 0 || email.value == null || email.value.length < 3 || !emailValidation.test(email.value)) {
+        e.preventDefault();
+        errorMail.style.display = 'block';
+        email.classList.add('invalidInput');
+    };
 }
--- js/shop.js
+++ js/shop.js
@@ -81,33 +81,83 @@ var total = 0;
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+    for (let i = 0; i < products.length; i++){
+        if (id === products[i].id) {
+            cartList.push(products[i]);
+        }
+    }
+    return cartList;
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList.length = 0;
 }
 
 // Exercise 3
 function calculateSubtotals() {
-    // 1. Create a for loop on the "cartList" array 
+    // 1. Create a for loop on the "cartList" array
     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+    for (let i = 0; i < cartList.length; i++){
+        switch (cartList[i].type) {
+            case 'grocery': subtotal['grocery'].value += cartList[i].price;
+                break;
+            case 'beauty': subtotal['beauty'].value += cartList[i].price;
+                break;
+            case 'clothes': subtotal['clothes'].value += cartList[i].price;
+        }
+    }
+    return subtotal;
 }
 
 // Exercise 4
 function calculateTotal() {
     // Calculate total price of the cart either using the "cartList" array
+   for (let i = 0; i < cartList.length; i++){
+        total += cartList[i].price
+    }
+    return total;
 }
 
 // Exercise 5
 function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
+    // Using the "cartlist" array that contains all the items in the shopping cart,
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    cartList = cartList.map(item => ({ ...item, quantity: 1, subtotalWithDiscount: null }));
+    cart = cartList.reduce((acc, value) => {
+        const itemDuplicated = acc.find(item => item.id === value.id);
+        if (itemDuplicated) {
+            return acc.map((item) => {
+                if (item.id === value.id) {
+                    return {
+                        ...item,
+                        quantity: item.quantity + value.quantity
+                    }
+                }
+                return item;
+            });
+        }
+        return [...acc, value];
+    }, []);
+    return cart = cart.map(item => ({ ...item, subtotal: item.quantity * item.price }));
 }
 
 // Exercise 6
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    generateCart();
+    
+   for (let i = 0; i < cart.length; i++){
+       if (cart[i].name === 'cooking oil' && cart[i].quantity > 2) {
+           cart[i].subtotalWithDiscount = (cart[i].price - 0.5) * cart[i].quantity; 
+       }
+
+       if (cart[i].name === 'Instant cupcake mixture' && cart[i].quantity > 9) {
+           cart[i].subtotalWithDiscount = (((cart[i].price * 2) / 3) * cart[i].quantity);
+       }
+    }
+
+    return cart;
 }
 
 // Exercise 7
