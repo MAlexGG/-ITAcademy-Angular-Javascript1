aa373352c382f2b8f9d0c34e1f26a45c61cdf67a
Alexandra Galarza <alexandragalarzag@gmail.com>
2022-01-28 19:42:50 +0100

    checkout page

--- level-3/js/checkout.js
+++ level-3/js/checkout.js
@@ -27,31 +27,37 @@ function validate(e) {
     if (password.value.length === 0 || password.value == null || password.value.length < 3 || !numbersAndLetters.test(password.value)) {
         e.preventDefault();
         errorPassword.style.display = 'block';
+        errorPassword.style.fontWeight = 'bold';
         password.classList.add('invalidInput');
     };
     if (firstName.value.length === 0 || firstName.value == null || firstName.value.length < 3 || numbers.test(firstName.value)) {
         e.preventDefault();
         errorName.style.display = 'block';
+        errorName.style.fontWeight = 'bold';
         firstName.classList.add('invalidInput');
     };
     if (lastName.value.length === 0 || lastName.value == null || lastName.value.length < 3 || numbers.test(lastName.value)) {
         e.preventDefault();
         errorLastName.style.display = 'block';
+        errorLastName.style.fontWeight = 'bold';
         lastName.classList.add('invalidInput');
     };
     if (phone.value.length === 0 || phone.value == null || phone.value.length < 3 || letters.test(phone.value)) {
         e.preventDefault();
         errorPhone.style.display = 'block';
+        errorPhone.style.fontWeight = 'bold';
         phone.classList.add('invalidInput');
     };
     if (address.value.length === 0 || address.value == null || address.value.length < 3 ) {
         e.preventDefault();
         errorAddress.style.display = 'block';
+        errorAddress.style.fontWeight = 'bold';
         address.classList.add('invalidInput');
     };
     if (email.value.length === 0 || email.value == null || email.value.length < 3 || !emailValidation.test(email.value)) {
         e.preventDefault();
         errorMail.style.display = 'block';
+        errorMail.style.fontWeight = 'bold';
         email.classList.add('invalidInput');
     };
 }
